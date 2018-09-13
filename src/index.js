d1ff6d35ec2dc4584e39c99d5c7881fd67ae7929
Francisco Morais <francisco.luis.morais@gmail.com>
2018-09-14 00:45:04 +0100

    Add initial script

---
+++ src/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+import { install } from './commands';
+import program from 'commander';
+
+/**
+ * Version.
+ */
+
+program.version('1.0.0');
+
+/**
+ * `install` command.
+ */
+
+program
+  .command('install [library]')
+  .description('run this command to install flow-typed dependency')
+  .action(install);
+
+/**
+ * Show help if no pass any command.
+ */
+
+if (process.argv.length == 2) console.log(program.helpInformation());
+
+/**
+ * Parse arguments.
+ */
+
+program.parse(process.argv);
