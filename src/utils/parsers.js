54ab10b59567a9a37d0617c9d1a99af25cb69b19
Francisco Morais <francisco.luis.morais@gmail.com>
2018-09-23 01:28:26 +0100

    Fix parsers utils
    Separate into two methods to be able to test code separately.

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -6,23 +6,36 @@ import { get } from 'lodash';
 import { yarnList } from 'utils/shell-commands';
 
 /**
+ * Export `parseYarnResult` util.
+ */
+
+export const parseYarnResult = data => {
+  const result = get(data, 'data.trees.0.name');
+  const splitLibrary = result ? result.split('@') : [];
+
+  return result ? {
+    result,
+    splitLibrary: splitLibrary.length === 2 ? splitLibrary : null
+  } : {};
+};
+
+/**
  * Export `checkLibrary` util.
  */
 
 export async function checkLibrary(library) {
+  if (!library) {
+    return null;
+  }
+
   const splitLibrary = library.split('@');
+  const libraryName = splitLibrary.length >= 2 ? splitLibrary[0] : library;
 
-  if (splitLibrary.length < 2) {
-    try {
-      const json = await yarnList(library);
-      const result = get(json, 'data.trees.0.name');
+  try {
+    const json = await yarnList(libraryName);
 
-      return result ? {
-        result,
-        splitLibrary: result ? result.split('@') : null
-      } : {};
-    } catch(e) {
-      return null;
-    }
+    return parseYarnResult(json);
+  } catch(e) {
+    return null;
   }
 };
